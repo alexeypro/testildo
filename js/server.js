81b815b35a569c940818cead3974104d5b3da226
Alexey Prohorenko <ap@alexeypro.com>
2012-03-27 13:02:47 -0700

    now it can insert data into mongo on every insert request

--- js/server.js
+++ js/server.js
@@ -1,41 +1,54 @@
-var http            =   require('http')
-  , mongoose        =   require('mongoose')
-  , express         =   require('express')
-  , SERVER_PORT     =   8181
-  , MONGODB_DB      =   'testildodb'
-  , MONGODB_USER    =   'testildouser'
-  , MONGODB_PASS    =   'testildopwd'
-  , MONGODB_URI     =   'mongodb://' + MONGODB_USER + ':' + MONGODB_PASS + '@ds031637.mongolab.com:31637/' + MONGODB_DB
-  , Schema          =   mongoose.Schema;
+var http            =   require('http'),
+    async           =   require('async'),
+    mongoose        =   require('mongoose'),
+    express         =   require('express'),
+    SERVER_PORT     =   8181, 
+    MONGODB_DB      =   'testildodb', 
+    MONGODB_USER    =   'testildouser', 
+    MONGODB_PASS    =   'testildopwd', 
+    MONGODB_URI     =   'mongodb://' + MONGODB_USER + ':' + MONGODB_PASS + '@ds031637.mongolab.com:31637/' + MONGODB_DB;
 
+
+var TestJSRecord    =   new mongoose.Schema( { title : String, body : String } ),
+    INSERT_COUNT    =   500;      // how many records to insert on every /insert call
 
 console.log('Connecting to MongoDB; ' + MONGODB_URI + '...');
 var mongoConn = mongoose.createConnection(MONGODB_URI);
-console.log('Connected!');
+var TestJSRecordModel = mongoConn.model('TestJSRecord', TestJSRecord);
+console.log('Connected and model initialized!');
 
-// ---
-var TestRecord = new Schema({
-    title     : String
-  , body      : String
-});
-mongoConn.model('TestRecord', TestRecord);
-// TODO: generate a bunch of random models
-var m = new TestRecord();
-m.title = 'random title 1';
-m.body = 'random body 1';
-m.save();
-// ---
-
+// make sure app is aware of all the global stuff
 var app = express.createServer();
+app._connections = { 'mongoConn': mongoConn };
+app._models = { 'TestJSRecordModel': TestJSRecordModel };
 
 app.get('/', function(req, res){
-    console.log('request /');
+    console.log('Request /');
     res.send('Hello World');
 });
 
-app.get('/insert', function(req, res){
-    console.log('request /insert');
+app.get('/insert', function(req, res) {
+    console.log('Request /insert');
     res.send('Inserting random record');
+    // TODO: generate a bunch of random models
+    var count = 0;
+    async.whilst(
+        function continueWhileTrue() { 
+            return count < INSERT_COUNT; 
+        },
+        function executeTask(callback) {
+            count++;
+            var ts = Math.round((new Date()).getTime());
+            var m = new TestJSRecordModel();
+            m.title = 'random title ' + count + ' @ ' + ts;
+            m.body = 'random body ' + count + ' @ ' + ts;
+            m.save();            
+            callback();
+        },
+        function doneExecution(err) {
+            console.log('Finished inserting ' + count + ' record(s)!');
+        }
+    );    
 });
 
 app.listen(SERVER_PORT);
