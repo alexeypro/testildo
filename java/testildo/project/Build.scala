d1b6d6208d6865946484d9bfac9344a3c6cb0392
Alexey Prohorenko <ap@alexeypro.com>
2012-03-27 23:04:03 -0700

    using resolvers for library dependencies

--- java/testildo/project/Build.scala
+++ java/testildo/project/Build.scala
@@ -9,11 +9,13 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
       // Add your project dependencies here,
+      "com.google.code.morphia" % "morphia-logging-slf4j" % "0.99",
       "com.google.code" % "morphia" % "0.91"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
       // Add your own project settings here      
+      resolvers += "Morphia repository" at "http://morphia.googlecode.com/svn/mavenrepo/"
     )
 
 }
