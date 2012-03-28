82425e4acd89777cf5ed1a87caef6ef1ce6d54c8
Alexey Prohorenko <ap@alexeypro.com>
2012-03-27 23:04:27 -0700

    planning to use it for initial initialization -- probably from scala stuff

---
+++ java/testildo/app/Startup.java
@@ -0,0 +1,11 @@
+
+import com.google.code.morphia.logging.MorphiaLoggerFactory;
+//import com.google.code.morphia.logging.slf4j.SLF4JLoggerImplFactory;
+
+public class Startup {
+	
+	public final static void initialize() {
+		//MorphiaLoggerFactory.registerLogger(SLF4JLoggerImplFactory.class);
+	}
+
+}
