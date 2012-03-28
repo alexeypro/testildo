74a0a5ae94c4a37e2ffbcdac190050fa8cf505b1
Alexey Prohorenko <ap@alexeypro.com>
2012-03-27 22:23:48 -0700

    using morphia library dependency

--- java/testildo/project/Build.scala
+++ java/testildo/project/Build.scala
@@ -9,6 +9,7 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
       // Add your project dependencies here,
+      "com.google.code" % "morphia" % "0.91"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
